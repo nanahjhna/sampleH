975964cb2f0215a10fc2dd96686817ea46ec3e34
nanahjhna <nanahjhna7@naver.com>
2024-12-23 16:12:34 +0900

    20241223 index.js hello world 추가

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
-/**
- * @format
- */
+// Import the React and React Native libraries
+import React from 'react';
+import { AppRegistry, Text, View } from 'react-native';
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+// Create a React component
+const App = () => {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Hello World!!!</Text>
+    </View>
+  );
+};
 
-AppRegistry.registerComponent(appName, () => App);
+// Register the component with the AppRegistry
+AppRegistry.registerComponent('sampleH', () => App);
